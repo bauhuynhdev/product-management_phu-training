26f74d454ba8e964c411df13268eee51d740bbfb
Phu <lehongphu18i1@gmail.com>
2021-11-14 18:54:37 +0700

    finish-create-form-login-and-register

---
+++ resources/views/login.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="/frontend/css/login.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form đăng nhập</title>
+</head>
+<body>
+    <div class="container">
+        <form class="form-login">
+            <h1>Đăng nhập</h1>
+            <div class="form-text">
+                <input type="text" placeholder="Email">
+            </div>
+            <div class="form-text">
+                <input type="password" placeholder="Password">
+            </div>
+            <button>Đăng nhập</button>
+            <span>Bạn chưa có tài khoản? <a href="#">Đăng ký tại đây</a></span>
+        </form>
+    </div>
+</body>
+</html>
---
+++ resources/views/register.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="/frontend/css/register.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form đăng nhập</title>
+</head>
+<body>
+    <div class="container">
+        <form class="form-login">
+            <h1>Đăng Ký</h1>
+            <div class="form-text">
+                <input type="text" placeholder="Họ và tên">
+            </div>
+            <div class="form-text">
+                <input type="password" placeholder="Email">
+            </div>
+            <div class="form-text">
+                <input type="password" placeholder="Mật Khẩu">
+            </div>
+            <div class="form-text">
+                <input type="password" placeholder="Xác nhận mật khẩu">
+            </div>
+            <button>Đăng ký</button>
+            <span>Bạn đã có tài khoản? <a href="#">Đăng nhập tại đây</a></span>
+        </form>
+    </div>
+</body>
+</html>
