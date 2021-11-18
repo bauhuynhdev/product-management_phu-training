3267ad837c4bf580bb7cca81319f8a2aa08a728a
Phu <lehongphu18i1@gmail.com>
2021-11-18 18:34:21 +0700

    hoan thanh bai tap

---
+++ resources/views/add.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/frontend/css/add.css">
+    <title>Form thêm mới</title>
+</head>
+<body>
+<div class="container">
+    <div>
+        <section class="section clearfix">
+            <div>
+                <div class="seccontactform">
+                    <form action="" method="">
+                        <div class="clearfix">
+                            <input class="col2 first" type="text" name="" placeholder="Tên sản phẩm" value="">
+                        </div>
+                        <div class="clearfix">
+                            <input class="col2 first" type="file" name="" placeholder="Chọn ảnh" value="">
+                        </div>
+                        <div class="clearfix">
+                            <input class="col2 first" type="text" name="" placeholder="Mã sản phẩm" value="">
+                        </div>
+                        <div class="clearfix">
+                            <input class="col2 first" type="text" name="" placeholder="Giá" value="">
+                        </div>
+                        <div class="clearfix">
+                            <input type="submit" value="Thêm mới">
+                            <a href="#" class="buttons">Về trang quản trị</a>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </section>
+
+    </div>
+</div>
+</body>
+</html>
---
+++ resources/views/admin.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/frontend/css/admin.css">
+    </head>
+    <body>
+            <div id="admin-heading-panel">
+                <div class="container">
+                    <div class="left-panel">
+                        Xin chào <span>Admin</span>
+                    </div>
+                    <div class="right-panel">
+                        <img height="24" src="home icon (1).jpg" />
+                        <a href="#">Trang chủ</a>
+                        <img height="24" src="500_F_33527214_1ySp8kyGMZEAgk5MGELVAFxHxXRPNVEE.jpg" />
+                        <a href="#">Đăng xuất</a>
+                    </div>
+                </div>
+            </div>
+            <div id="content-wrapper">
+                <div class="container">
+                    <div class="left-menu">
+                        <div class="menu-heading">Admin Menu</div>
+                        <div class="menu-items">
+                            <ul>
+                                <li><a href="product_listing.php">Sản phẩm</a></li>
+                                <li><a href="order_listing.php">Đơn hàng</a></li>
+                                <li><a href="member_listing.php">Quản lý thành viên</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="main-content">
+                        <h1>Danh sách sản phẩm</h1>
+                        <div class="listing-items">
+                            <div class="buttons">
+                                <a href="#">Thêm mới</a>
+                            </div>
+                            <div class="listing-search">
+                                <form>
+                                    <fieldset>
+                                        <legend>Tìm kiếm</legend><input type="text">
+                                        <input type="submit" value="Tìm" />
+                                    </fieldset>
+                                </form>
+                            </div>
+                            <table>
+                                <tr>
+                                    <th>Tên sản phẩm</th>
+                                    <th>Ảnh sản phẩm</th>
+                                    <th>Mã sản phẩm</th>
+                                    <th>Giá</th>
+                                    <th>Copy</th>
+                                    <th>Sửa</th>
+                                    <th>Xoá</th>
+                                </tr>
+                                <tr>
+                                    <td>YZF</td>
+                                    <td><img height="70px" src="yzf-r1374b (1).jpg" /></td>
+                                    <td>14141242</td>
+                                    <td>100,000,000VND</td>
+                                    <td><a href="#">Copy</a></td>
+                                    <td><a href="#">Sửa</a></td>
+                                    <td><a href="#">Xoá</a></td>
+                                </tr>
+                                <tr>
+                                    <td>YZF</td>
+                                    <td><img height="70px" src="yzf-r1374b (1).jpg" /></td>
+                                    <td>14141242</td>
+                                    <td>100,000,000VND</td>
+                                    <td><a href="#">Copy</a></td>
+                                    <td><a href="#">Sửa</a></td>
+                                    <td><a href="#">Xoá</a></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+    </body>
+</html>
+
